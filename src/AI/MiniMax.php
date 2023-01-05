1e3fb836bd803e120efc27d3d1bd7ca3789c1c5b
Amashige Seiji <tenjuu99@gmail.com>
2023-01-06 05:22:36 +0900

    update

--- src/AI/MiniMax.php
+++ src/AI/MiniMax.php
@@ -7,13 +7,13 @@ use Tenjuu99\Reversi\Model\Move;
 
 class MiniMax implements ThinkInterface
 {
-    private $searchLevel = 4;
+    private $searchLevel = 3;
     private History $history;
 
     public function choice(Game $game) : ?Move
     {
         $this->history = $game->toHistory();
-        $choice = $this->miniMax($game, $this->searchLevel, $game->computerPlayer() === $game->getCurrentPlayer());
+        $choice = $this->miniMax($game, $this->searchLevel, true);
         $game->fromHistory($this->history);
         return $choice === 'pass' ? null : $choice;
     }
@@ -21,15 +21,15 @@ class MiniMax implements ThinkInterface
     /**
      * @param Game $game
      * @param int $depth 探索する深さ
-     * @param bool $flagComputerTurn コンピュータが攻めのときは true
+     * @param bool $flag 評価側を true, 敵側を false
      */
-    public function miniMax(Game $game, int $depth, bool $flagComputerTurn)
+    public function miniMax(Game $game, int $depth, bool $flag)
     {
         if ($depth === 0) {
             // score を返す
             return Evaluator::score($game, $game->computerPlayer());
         }
-        $value = $flagComputerTurn ? PHP_INT_MIN : PHP_INT_MAX;
+        $value = $flag ? PHP_INT_MIN : PHP_INT_MAX;
         $bestIndex = null;
 
         $moves = $game->moves();
@@ -43,8 +43,8 @@ class MiniMax implements ThinkInterface
             } else {
                 $game->move($index);
             }
-            $childValue = $this->miniMax($game, $depth - 1, !$flagComputerTurn);
-            $condition = $flagComputerTurn ? $value < $childValue : $value > $childValue;
+            $childValue = $this->miniMax($game, $depth - 1, !$flag);
+            $condition = $flag ? $value < $childValue : $value > $childValue;
             if ($condition) {
                 $value = $childValue;
                 $bestIndex = $move;
